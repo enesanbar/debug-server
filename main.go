718b62125917dd6805cfed5666153669e48daa40
Enes Anbar <enesanbar@gmail.com>
2025-10-23 16:02:17 +0300

    add version

--- main.go
+++ main.go
@@ -10,42 +10,43 @@ import (
 )
 
 type DebugInfo struct {
-	Timestamp      string            `json:"timestamp"`
-	Method         string            `json:"method"`
-	Path           string            `json:"path"`
-	RemoteAddr     string            `json:"remote_addr"`
-	Headers        map[string]string `json:"headers"`
-	QueryParams    map[string]string `json:"query_params"`
-	Host           string            `json:"host"`
-	ServerInfo     ServerInfo        `json:"server_info"`
+	Timestamp   string            `json:"timestamp"`
+	Method      string            `json:"method"`
+	Path        string            `json:"path"`
+	RemoteAddr  string            `json:"remote_addr"`
+	Headers     map[string]string `json:"headers"`
+	QueryParams map[string]string `json:"query_params"`
+	Host        string            `json:"host"`
+	ServerInfo  ServerInfo        `json:"server_info"`
+	Version     string            `json:"version"`
 }
 
 type ServerInfo struct {
-	Hostname       string `json:"hostname"`
-	GoVersion      string `json:"go_version"`
-	NumCPU         int    `json:"num_cpu"`
-	NumGoroutine   int    `json:"num_goroutine"`
-	OS             string `json:"os"`
-	Architecture   string `json:"architecture"`
+	Hostname     string `json:"hostname"`
+	GoVersion    string `json:"go_version"`
+	NumCPU       int    `json:"num_cpu"`
+	NumGoroutine int    `json:"num_goroutine"`
+	OS           string `json:"os"`
+	Architecture string `json:"architecture"`
 }
 
 func debugHandler(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
-	
+
 	headers := make(map[string]string)
 	for name, values := range r.Header {
 		if len(values) > 0 {
 			headers[name] = values[0]
 		}
 	}
-	
+
 	queryParams := make(map[string]string)
 	for key, values := range r.URL.Query() {
 		if len(values) > 0 {
 			queryParams[key] = values[0]
 		}
 	}
-	
+
 	debugInfo := DebugInfo{
 		Timestamp:   time.Now().UTC().Format(time.RFC3339),
 		Method:      r.Method,
@@ -62,20 +63,21 @@ func debugHandler(w http.ResponseWriter, r *http.Request) {
 			OS:           runtime.GOOS,
 			Architecture: runtime.GOARCH,
 		},
+		Version: "v2",
 	}
-	
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	
+
 	json.NewEncoder(w).Encode(debugInfo)
 }
 
 func main() {
 	http.HandleFunc("/", debugHandler)
-	
+
 	port := "8080"
 	log.Printf("Starting server on port %s", port)
-	
+
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
